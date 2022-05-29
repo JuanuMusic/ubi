ed741041d70c1a45c54c8ff5ab32cf648c857bc2
Juanu <juanu@juanu.com.ar>
2022-05-29 12:24:13 +0200

    Deploy fubi script updated

--- scripts/deploy-fubi.js
+++ scripts/deploy-fubi.js
@@ -11,28 +11,19 @@ async function main() {
 
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
-    const Subi = await ethers.getContractFactory("fUBI");
-    console.log("Deploying sUBI");
+    const UBI_ADDRESS = "0xf261c4f93d1b2991bB47e7F295AF3B2Fc17BD440";
+    const FUBI_GOVERNOR = "0x2ad91063e489CC4009DF7feE45C25c8BE684Cf6a";
+    const FUBI_MAX_DELEGATIONS = 100;
+    const FUBI_NAME = "Flow UBI";
+    const FUBI_SYMBOL = "FUBI";
 
-    // const token = await upgrades.deployProxy(
-    //   Token,
-    //   [
-    //     deploymentParams.INITIAL_SUPPLY,
-    //     deploymentParams.TOKEN_NAME,
-    //     deploymentParams.TOKEN_SYMBOL,
-    //     deploymentParams.ACCRUED_PER_SECOND,
-    //     deploymentParams.PROOF_OF_HUMANITY_KOVAN
-    //   ],
-    //   {
-    //     initializer: 'initialize',
-    //     unsafeAllowCustomTypes: true 
-    //   }
-    // );
-    const subi = await Subi.deploy("0x1ac5F168C220De2515Af6068c5A153aFe2c76d36", "0x2ad91063e489CC4009DF7feE45C25c8BE684Cf6a", 100, "Flow UBI", "FUBI");
+    const FUBI = await ethers.getContractFactory("fUBI");
+    console.log("Deploying fUBI");
+    const fubi = await FUBI.deploy(UBI_ADDRESS, FUBI_GOVERNOR, FUBI_MAX_DELEGATIONS, FUBI_NAME, FUBI_SYMBOL);
 
-    await subi.deployed()
+    await fubi.deployed()
 
-    console.log("SUBI deployed to:", subi.address);
+    console.log("FUBI deployed to:", fubi.address);
 }
 
 main()
